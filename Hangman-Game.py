7fce4055fd7034a1ce7561a0b139da8ae324827f
Epicskylegend <adeba003@gmail.com>
2023-05-10 19:23:10 -0500

    Hangman project

---
+++ Hangman-Game.py
@@ -0,0 +1,36 @@
+import random
+class  Hangman:
+    words = ["pizza", "flower", "cherry"]
+    randWord = random.choice(words)
+    newWord = 1
+    spacing = []
+
+    randList = randWord.split(",")
+    print(randList)
+    print("\n")
+    
+   
+    print("Guess a letter for the word below")
+    """while newWord != randWord:   
+
+        print(randWord)
+        for i in range(len(randWord)):
+           if i < len(randWord):
+                spacing += "_"
+                i+=1
+                newSpacing = ''.join(spacing)
+        print(newSpacing) """
+    
+    
+    validInput = input(randWord)
+    if validInput.isalpha and len(validInput) == 1:
+        print("The letter you guess is " + validInput + ".")
+    else:
+        print("Invalid input.")
+        print("yes")
+        
+
+  
+    #print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+
+   
