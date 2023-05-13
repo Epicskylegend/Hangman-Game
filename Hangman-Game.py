0f63328048756ffdcc2d4d8b6c7738acbd64bcd3
Epicskylegend <adeba003@gmail.com>
2023-05-12 20:05:29 -0500

    Added feature to tell user exactly how many letters are in the word when they guess a correct letter. Also fixed bugs.

--- Hangman-Game.py
+++ Hangman-Game.py
@@ -13,7 +13,6 @@ class Hangman:
     lives = 7
    
     
-
     subtract = len(randWord) /2 
     math.floor(subtract)
     numLettersToRemove =  math.floor(subtract)
@@ -38,7 +37,7 @@ class Hangman:
 
 
         
-        missingCharacters[wordToSolve[indexReplaced]] = missingCharacters.get(wordToSolve[indexReplaced], []) + [indexReplaced] 
+        missingCharacters[wordToSolve[indexReplaced]] = missingCharacters.get(wordToSolve[indexReplaced], []) + [indexReplaced] # List of values inside dictionary
         #print(missingCharacters)
 
         randNumList.pop(letterToReplace)
@@ -53,33 +52,49 @@ class Hangman:
     
     print("Guess a letter for the word above\n")
     while endResult != randWord:   
-    
+       
     
         validLetter = input("Guess a letter: ")
+        
         
        
         if validLetter.isalpha and len(validLetter) == 1:
             print("The letter you guess is: " + validLetter + ".")
+
+        
+
+
 
         elif validLetter.isalpha() and len(validLetter) > 1:
             print("Invalid input. You can only enter in one letter at a time.\n")
 
 
 
-        if validLetter in missingCharacters.keys():
-            print("There is at least 1 " + validLetter + " missing inside of this word.\n")
+        if validLetter in missingCharacters.keys() and len(missingCharacters[validLetter]) == 1:
+            print("There is 1 " + '"' + validLetter + '"' + " missing from this word.\n")
 
             for i in (missingCharacters[validLetter]):
+                
                 wordToSolve[i] = validLetter
-            
-
             missingCharacters.pop(validLetter)
-    
-            
-          
             print("The word now looks like:")
             print(wordToSolve)
             print("\n")
+
+
+        elif validLetter in missingCharacters.keys() and len(missingCharacters[validLetter]) > 1:
+            print("There are " + '"' + str(len(missingCharacters[validLetter])) + '" ' +  validLetter + "'s" + " missing from the word.")
+
+
+            for i in (missingCharacters[validLetter]):
+                wordToSolve[i] = validLetter   
+
+            missingCharacters.pop(validLetter)
+            print("The word now looks like:")
+            print(wordToSolve)
+            print("\n")
+
+
            
         elif not validLetter.isalpha():
             print( str(validLetter) + " is not a valid input. Please enter in a letter.""\n")
@@ -87,9 +102,9 @@ class Hangman:
             print(wordToSolve)
             print("\n")
 
-        elif validLetter.isalpha and validLetter not in missingCharacters.keys():
+        elif validLetter.isalpha and len(validLetter) == 1 and validLetter not in missingCharacters.keys():
             print("There are NOT any " + validLetter + "'s missing inside of this word. You lose a life.\n")
-            print("The word currently looks like: ")
+            print("The word currently looks like:")
             print(wordToSolve)
             print("\n")
             lives -= 1 
@@ -100,7 +115,7 @@ class Hangman:
 
 
         if missingCharacters == {}:
-            print("You solved the word " + '"' + randWord + '"'  + " congratulations.\n")
+            print("You solved the word " + '"' + randWord + '"'  + ", congratulations.\n")
             raise SystemExit
         
         if lives == 0:
